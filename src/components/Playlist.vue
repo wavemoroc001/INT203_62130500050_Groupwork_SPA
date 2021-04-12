70bd2fc6f9fbf99963ea8485fa4dc2b0ff4abe9b
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-04-12 19:58:25 +0700

    Test add tailwindcss

---
+++ src/components/Playlist.vue
@@ -0,0 +1,5 @@
+<template>
+
+    <nav class="min-w-full p-5 bg-white shadow-md h-13 text-salmon">I'm Navigation bar!</nav>
+    <!-- <p1 class="text-5xl justify-items-center"> Hello from component</p1> -->
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Playlist from '../components/Playlist.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'playlist',
+    component: Playlist
   },
   {
     path: '/about',
@@ -14,6 +15,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
 ]
 
