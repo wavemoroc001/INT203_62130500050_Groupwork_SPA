9a003e1594b5e00a1aac1319a11031a26fd5db09
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-04-15 01:14:14 +0700

    62130500050  finish ui bookingFlights components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="w-full p-2 bg-white shadow-md h-16 flex justify-evenly">
+
+    <a href="/" class="flex ml-36">
+      <img :src="require('../assets/icon/icon.svg')" class="h-12" />
+      <span class="ml-3 nb">Flights Scanner</span>
+    </a>
+    
+    <a class="nb" href="/"><span>Home</span></a>
+    <a class="nb" href="/about"><span>About</span></a>
+    <a class="nb mr-36" href="/booking-flight"><span>Booking flight</span></a>
+ 
+  </nav>
+</template>
+<script>
+export default {};
+</script>
--- src/components/Playlist.vue
+++
@@ -1,5 +0,0 @@
-<template>
-
-    <nav class="min-w-full p-5 bg-white shadow-md h-13 text-salmon">I'm Navigation bar!</nav>
-    <!-- <p1 class="text-5xl justify-items-center"> Hello from component</p1> -->
-</template>
