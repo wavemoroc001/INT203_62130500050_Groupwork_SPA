a1701784652cb9571bd7f95815e297a2d611c00b
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-04-16 13:14:05 +0700

    62130500050 SearchFlight Finish

---
+++ src/components/DisplayFlights.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid grid-rows-3">
+    <img src="../assets/AirlineLogo/airasia.png" class="h-24" />
+    <div class="grid grid-col-3">
+      <span>Depart : 02.00 AM</span>
+      <span>Arrived : 09.45 AM</span>
+      <span>Total Traveltime : 20 Hours 20 min</span>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    filterFlights: {
+      type: Array,
+      require: true,
+    },
+  },
+};
+</script>
--- src/views/BookingFlight.vue
+++ src/views/BookingFlight.vue
@@ -1,26 +1,54 @@
 <template>
-  <div class="min-w-screen min-h-screen relative">
+  <div class="min-w-full h-screen flex flex-col relative justify-center items-center">
     <nav-bar />
     <img
-      class="opacity-75 min-w-full min-h-full bg-cover"
+      class="opacity-75 w-full h-full object-cover"
       src="../assets/bg/airplane-taking-off-from-airport.jpeg"
     />
-    <h3 class="text-5xl text-black absolute top-80 left-96">
+    <h3 class="text-5xl font-semibold text-black absolute top-64 left-96">
       Let the journey begin
     </h3>
-    <search-flight class="absolute top-96 left-96" />
+    <search-flight
+      class="absolute top-80 left-96"
+      @search-flight="filterFlights"
+    />
     <!-- <search-flight class="absolute top-2/4 left-2/4 transform -translate-x-2/4 -translate-y-2/4" /> -->
+  </div>
+  <div class="min-w-full h-screen flex flex-col relative justify-center items-center">
+    <display-flights class=" rounded-lg bg-blue-300 shadow-lg" :filterFlights="flights"/>
   </div>
 </template>
 <script>
 import SearchFlight from "../components/SearchFlight.vue";
+import DisplayFlights from "../components/DisplayFlights.vue";
 export default {
   components: {
     SearchFlight,
-  },data() {
-      return {
-         
-      }
+    DisplayFlights
+  },
+  data() {
+    return {
+      flights: [],
+    };
+  },
+  methods: {
+    async fetchflights() {
+      const res = await fetch("http://localhost:5000/fligths");
+      const data = res.json();
+      return data;
+    },
+    async filterFlights(flightsCriteria) {
+      this.flights = await this.fetchflights();
+      this.flights = this.flights.filter( function(flights){
+        if(flights.ticketType == flightsCriteria.ticketType &&
+           flights.from == flightsCriteria.from &&
+           flights.to == flightsCriteria.to &&
+           flights.departDate == flightsCriteria.departDate &&
+           flights.returnDate == flightsCriteria.returnDate &&
+           flights.cabinClass == flightsCriteria.cabinClass) return true;
+        return false;
+      });
+    },
   },
 };
 </script>
